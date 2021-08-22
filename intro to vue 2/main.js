db782f984cb1287d717987cdc6a5a7d4ebde583f
Jorge Falcón <jorgef_campos@outlook.com>
2021-08-21 20:41:24 -0500

    Computed properties

--- intro to vue 2/main.js
+++ intro to vue 2/main.js
@@ -1,13 +1,14 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Sneakers',
-        description: 'Nike Air Zoom',
-        image: './assets/nike_air_zoom_white.png',
+        brand: 'Nike',
+        product: 'Air Zoom',
+        description: 'Train harder. Leave a lighter footprint.',
+        selectedVariant: 0,
+        // onSaleProduct: true,
+        // image: './assets/nike_air_zoom_white.png',
         url: 'https://www.nike.com/mx/t/calzado-de-carrera-air-zoom-alphafly-next-flyknit-xDM1Lj?cp=41842406843_ad_!14129893903!128449409514!c!pla-324799387802!537150223802&cp=82486655958_search_%7c%7c14129893903%7c128449409514%7c%7cc%7c%7c%7c537150223802&gclid=Cj0KCQjwpf2IBhDkARIsAGVo0D1yY9lDTAv5FrofgqdP1nBPtDnk6mv7cC145HAJXtU4oZGS-eZ9eKkaArTyEALw_wcB&gclsrc=aw.ds',
-        inStock: false,
         inventory: 11,
-        onSale: true,
         details : [
             "Placa de fibra de carbono",
             "Solo 210 gramos de peso",
@@ -18,11 +19,15 @@ var app = new Vue({
                 variantId: 2234,
                 variantColor: "white",
                 variantImage: './assets/nike_air_zoom_white.png',
+                variantStockQuantity: 10,
+                onSaleVariant: true
             },
             {
                 variantId: 2235,
                 variantColor: "black",
                 variantImage: './assets/nike_air_zoom_black.png',
+                variantStockQuantity: 0,
+                onSaleVariant: false
             }
         ],
 
@@ -35,6 +40,7 @@ var app = new Vue({
             28
         ],
         cart: 0
+
     
     },
     methods: {
@@ -46,11 +52,32 @@ var app = new Vue({
             this.cart -= 1;
         },
 
-        updateProductPreview(variantImage) {
-            this.image = variantImage
+        updateProductPreview(index) {
+            this.selectedVariant = index
+            console.log(index);
         }
 
 
+    },
+
+    computed: {
+        title() {
+            return `${this.brand} ${this.product}`;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantStockQuantity;
+        },
+        isOnSale() {
+            if (this.variants[this.selectedVariant].onSaleVariant) {
+                return `${this.title} in variant ${this.variants[this.selectedVariant].variantColor} are on sale`
+            }
+            else {
+                return null;
+            }
+        }
     }
     
 
