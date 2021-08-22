da9e9542aede92f011508e133d8367181c9157a7
Jorge Falcón <jorgef_campos@outlook.com>
2021-08-21 20:00:59 -0500

    Added remove from cart method and improved views

--- intro to vue 2/main.js
+++ intro to vue 2/main.js
@@ -5,6 +5,7 @@ var app = new Vue({
         description: 'Nike Air Zoom',
         image: './assets/nike_air_zoom_white.png',
         url: 'https://www.nike.com/mx/t/calzado-de-carrera-air-zoom-alphafly-next-flyknit-xDM1Lj?cp=41842406843_ad_!14129893903!128449409514!c!pla-324799387802!537150223802&cp=82486655958_search_%7c%7c14129893903%7c128449409514%7c%7cc%7c%7c%7c537150223802&gclid=Cj0KCQjwpf2IBhDkARIsAGVo0D1yY9lDTAv5FrofgqdP1nBPtDnk6mv7cC145HAJXtU4oZGS-eZ9eKkaArTyEALw_wcB&gclsrc=aw.ds',
+        inStock: false,
         inventory: 11,
         onSale: true,
         details : [
@@ -41,9 +42,12 @@ var app = new Vue({
             this.cart += 1;
         },
 
+        removeFromCart() {
+            this.cart -= 1;
+        },
+
         updateProductPreview(variantImage) {
             this.image = variantImage
-
         }
 
 
