840a59d64283ab31870240ea8dfccf3e8381eee3
Jorge Falcón <jorgef_campos@outlook.com>
2021-08-30 08:55:49 -0500

    Event handling

--- intro to vue 3/main.js
+++ intro to vue 3/main.js
@@ -2,6 +2,7 @@
 const app = Vue.createApp({
     data: function() {
         return {
+            cart:0,
             product: 'Socks',
             image: './assets/nike_air_zoom_white.PNG',
             url: 'https://www.nike.com/mx/t/calzado-de-carrera-air-zoom-alphafly-next-flyknit-xDM1Lj?cp=41842406843_ad_!14129893903!128449409514!c!pla-324799387802!537150223802&cp=82486655958_search_%7c%7c14129893903%7c128449409514%7c%7cc%7c%7c%7c537150223802&gclid=Cj0KCQjwpf2IBhDkARIsAGVo0D1yY9lDTAv5FrofgqdP1nBPtDnk6mv7cC145HAJXtU4oZGS-eZ9eKkaArTyEALw_wcB&gclsrc=aw.ds',
@@ -14,12 +15,35 @@ const app = Vue.createApp({
                 "Desplazamiento 4mm" 
             ],
             variants: [
-                {id: 2334, color: 'black'},
-                {id: 2335, color: 'white'},
+                {
+                    id: 2334, 
+                    color: 'black',
+                    image: './assets/nike_air_zoom_black.PNG',
+                },
+                {
+                    id: 2335, 
+                    color: 'white',
+                    image: './assets/nike_air_zoom_white.PNG',
+                },
             ],
             sizes: [
                 25, 26, 27, 28, 29
             ]
         }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1;
+        },
+        removeFromCart(){
+            if (this.cart >= 1){
+                this.cart -= 1;
+            }
+        },
+
+        updateImage(variantImage){
+            this.image = variantImage;
+        }
+
     }
 })
