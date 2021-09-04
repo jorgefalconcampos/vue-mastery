11776aefdff246c217e5f615ee0be4b3fc657c61
Jorge Falcón <jorgef_campos@outlook.com>
2021-09-04 00:18:56 -0500

    EventCard component

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="events">
+    <h1>Events for good</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "EventList",
   components: {
-    HelloWorld,
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 12345,
+          category: "animal welfare",
+          description: "Join us today!",
+          location: "People's park",
+          title: "Cat adoption",
+          date: "January 28, 2022",
+          time: "12: 00",
+          petsAllowed: true,
+          organizer: "Pepe Campa",
+        },
+        {
+          id: 100001,
+          category: "animal welfare",
+          description: "Join us today!",
+          location: "People's park",
+          title: "Cat adoption",
+          date: "January 28, 2022",
+          time: "12: 00",
+          petsAllowed: true,
+          organizer: "Pepe Campa",
+        },
+        {
+          id: 2332100001,
+          category: "animal welfare",
+          description: "Join us today!",
+          location: "People's park",
+          title: "Cat adoption",
+          date: "January 28, 2022",
+          time: "12: 00",
+          petsAllowed: true,
+          organizer: "Pepe Campa",
+        },
+      ],
+    };
   },
 };
 </script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
