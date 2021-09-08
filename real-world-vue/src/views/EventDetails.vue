d15fb7a49f90dab95eddaa6f0010819aa66b5cfb
Jorge Falcón <jorgef_campos@outlook.com>
2021-09-08 11:33:38 -0500

    Card details

---
+++ real-world-vue/src/views/EventDetails.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="event">
+      <h1>{{ event.title }}</h1>
+    <span> @ {{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.description }}</h4>
+  </div>
+</template>
+
+<script>
+
+import EventService from "@/services/EventService.js";
+
+export default {
+    props: ['id'],
+    data() {
+        return  {
+            event: null,
+        }
+    },
+    created() {
+        EventService.getEvent(this.id)
+        .then((response) => {
+            // console.log("events", response.data);
+            this.event = response.data; 
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+    }
+}
+</script>
